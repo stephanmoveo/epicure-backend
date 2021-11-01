14af8ebadad561338448cbcce587e39b099dc57b
Stephan benchemoul <stephanb@moveo.co.il>
2021-11-01 14:48:35 +0200

    routes

--- routes/RestaurantRoute.js
+++ routes/RestaurantRoute.js
@@ -4,9 +4,6 @@ const restaurantController = require('../controllers/RestaurantController')
 
 router.route('/createRestaurant').post(restaurantController.createRestaurant)
 router.route('/allRestaurants').get(restaurantController.allRestaurants)
-// router.route('/id').get(restaurantController.createRestaurant)
-// router.route('/create').post(restaurantController.allRestaurants)
-// router.route('/update').post(restaurantController.allRestaurants)
-// router.route('/delete').post(restaurantController.allRestaurants)
+
 module.exports = router
 
