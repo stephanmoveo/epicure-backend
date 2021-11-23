6593436fd5b9ca55b8bd19c15be4ead80c7d8db0
Stephan benchemoul <stephanb@moveo.co.il>
2021-11-23 16:26:28 +0200

    sdvdsv

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,8 @@ const userHandler = require("../handlers/UserHandler");
 
 exports.login = async (req, res, next) => {
   const data = req.body;
+  console.log(data);
+
   try {
     const token = await userHandler.login(data);
     if(!token)
