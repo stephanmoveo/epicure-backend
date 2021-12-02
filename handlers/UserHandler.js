ecdc0859506ae4afeaf2c2b2a4c66cbe1d606adb
Stephan benchemoul <stephanb@moveo.co.il>
2021-12-02 11:50:18 +0200

    redeployment

--- handlers/UserHandler.js
+++ handlers/UserHandler.js
@@ -1,7 +1,5 @@
 const userModel = require("../models/UserModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 exports.login = async (data) => {
   const user = await userModel
